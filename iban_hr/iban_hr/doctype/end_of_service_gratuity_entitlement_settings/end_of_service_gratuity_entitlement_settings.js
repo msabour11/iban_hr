76f9eb945d4d7c6db840ff346f31fbd3b899a816
IBAN <frappe@vmi2203412.contaboserver.net>
2025-07-19 11:13:23 +0200

    add doctype to module

---
+++ iban_hr/iban_hr/doctype/end_of_service_gratuity_entitlement_settings/end_of_service_gratuity_entitlement_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Mohamed AbdElsabour and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("End of Service Gratuity Entitlement Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
